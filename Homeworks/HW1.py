868b3dd1ebef697384f6cf103fbad453f5fdeee6
Hadjer Brioua <72814758+HadjerBr@users.noreply.github.com>
2021-04-07 19:30:43 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -1,5 +1,8 @@
-#Explain your work
+listim = [1, 5, 'Hadjer', 50.3]
+listim = listim[len(listim)//2:] + listim[:len(listim)//2]
+print(listim)
 
-#Question 1
-for x in range(a):
-	print(a)
+n = int(input("Enter a single digit integer: "))
+for number in range(n+1):
+    if number % 2 == 0:
+        print(number)
