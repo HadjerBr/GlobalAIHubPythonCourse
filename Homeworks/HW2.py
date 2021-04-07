1976c9d2b8cd8b1f4f3ed4da2f89a8ab0db99d28
Hadjer Brioua <72814758+HadjerBr@users.noreply.github.com>
2021-04-07 23:30:35 +0300

    Update HW2.py

--- Homeworks/HW2.py
+++ Homeworks/HW2.py
@@ -1,5 +1,12 @@
-#Explain your work
 
-#Question 1
-for x in range(a):
-	print(a)
+logInInformation = {'username1': 'password1', 'username2': 'password2', 'username3': 'password3'}
+name = input('Enter username: ')
+if name in logInInformation.keys():
+    password = input('Enter password: ')
+    if str(logInInformation[name]) == password:
+        print('Welcome', name)
+    else:
+        print('Invalid password')
+
+else:
+    print('Invalid user name')
