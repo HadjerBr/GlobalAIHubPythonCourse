c212b496702bfea2d9ba102f48df748479969d5d
Hadjer Brioua <72814758+HadjerBr@users.noreply.github.com>
2021-04-08 15:40:37 +0300

    Update HW1.py

--- Homeworks/HW1.py
+++ Homeworks/HW1.py
@@ -3,6 +3,9 @@ listim = listim[len(listim)//2:] + listim[:len(listim)//2]
 print(listim)
 
 n = int(input("Enter a single digit integer: "))
-for number in range(n+1):
-    if number % 2 == 0:
-        print(number)
+if n >= 0 and n < 10:
+    for number in range(n+1):
+        if number % 2 == 0:
+            print(number)
+else:
+    print('Invalid number')
