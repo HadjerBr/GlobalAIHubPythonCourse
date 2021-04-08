8bf18b4b9f849a16ab19300d9812d79374ff937f
Hadjer Brioua <72814758+HadjerBr@users.noreply.github.com>
2021-04-08 20:17:19 +0300

    Add files via upload

---
+++ Homeworks/homeWork1.py
@@ -0,0 +1,13 @@
+# Question 1
+listim = [1, 5, 'Hadjer', 50.3]
+listim = listim[len(listim)//2:] + listim[:len(listim)//2]
+print(listim)
+
+# Question 2
+n = int(input("Enter a single digit integer: "))
+if n >= 0 and n < 10:
+    for number in range(n+1):
+        if number % 2 == 0:
+            print(number)
+else:
+    print('Invalid number')
