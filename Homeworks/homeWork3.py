2ecb3e74aacf5bbce5a5aaea722aecb6ab442ec2
Hadjer Brioua <72814758+HadjerBr@users.noreply.github.com>
2021-04-10 15:52:47 +0300

    Add files via upload

---
+++ Homeworks/homeWork3.py
@@ -0,0 +1,28 @@
+students = []
+studentsByPassingGrades = []
+try:
+    for i in range(2):
+        info = input("\n\nEnter the student number:" + str(i+1) + "'s name, midterm grade, project grade and final grade with blanks between them: ").split()
+
+
+        name = info[0]
+        midterm = float(info[1])
+        project = float(info[2])
+        final = float(info[3])
+
+    #print("midterm, project and final grades should be numbers")
+
+        student = {"name": name, "midterm": midterm, "project": project, "final": final}
+        print("This student's information:\n ", student)
+        student["passing grade"] = passingGrade = round((student["midterm"]*0.3 + student["project"]*0.3 + student["final"]*0.4), 2)
+        students.append(student)
+        studentsByPassingGrades.append({'name': name, 'passing grade': passingGrade})
+
+
+    print("\n\nAll the students' marks and passing grades:\n", students)
+
+    studentsByPassingGrades.sort(key=lambda x: x.get('passing grade'), reverse=True)
+    print('\n\nAll the students from the highest passing grade to the lowest: \n', studentsByPassingGrades, end=' ')
+
+except:
+    print("midterm, project and final grades should be numbers")
